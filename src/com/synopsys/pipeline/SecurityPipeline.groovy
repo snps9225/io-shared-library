2eccf157d5b1ba98cda52727301be91e665a7de6
Meera Rao <18620983+meerasubbarao@users.noreply.github.com>
2022-01-07 10:39:01 -0500

    Update SecurityPipeline.groovy

--- src/com/synopsys/pipeline/SecurityPipeline.groovy
+++ src/com/synopsys/pipeline/SecurityPipeline.groovy
@@ -19,57 +19,57 @@ def execute() {
 			}
 			
 			stage('Building Source...'){
-				steps {
+				
 				   //your build command, your way here...    
 				   //sh '''mvn clean compile'''
 				   echo 'build source code'
-				}
+				
 			}
 		 
 			stage('IO - Setup Prescription') {
-				steps {
+				
 					 echo 'Setup Prescription'
 					 synopsysIO(connectors: [io(configName: 'iostaging', projectName: 'github-io-sample', workflowVersion: '2021.12.0.10-alpha'), 
 					 github(branch: 'production', configName: 'github-io-sample', owner: 'devsecops-test', repositoryName: 'github-io-sample'), 
 					 buildBreaker(configName: 'BB-ALL')]) {
 						sh 'io --stage io'
 					 }
-				}
+				
 			}
         
 			stage('SAST- RapidScan') {
 				environment {
 					OSTYPE='linux-gnu'   
 				}
-				steps {
+				
 				   echo 'Running SAST using Sigma - Rapid Scan'
 				   echo env.OSTYPE
 				   synopsysIO(connectors: [rapidScan(configName: 'Sigma')]) {
 					   sh 'io --stage execution --state io_state.json'
 				   }
-				} 
+				
 		    }   
 		  
 			stage('SAST - Polaris') {
-				steps {
+				
 				   echo 'Running SAST using Polaris'
 				   synopsysIO(connectors: [[$class: 'PolarisPipelineConfig', configName: 'csprod-polaris', projectName: 'sig-devsecops/github-io-sample']]) {
 					  sh 'io --stage execution --state io_state.json'
 				   }
-				}
+				
 			}
         
 			stage('SCA - Black Duck') {
-				steps {
+				
 				   echo 'Running SCA using Black Duck'
 				   synopsysIO(connectors: [blackduck(configName: 'BIZDevBD', projectName: 'devsecops-test/github-io-sample', projectVersion: '1.0')]) {
 					  sh 'io --stage execution --state io_state.json'
 				   }
-				}
+				
 			}
         
 			stage('IO - Workflow') {
-				steps {
+				
 					echo 'Execute Workflow Stage'
 					synopsysIO() {
 						synopsysIO(connectors: [slack(configName: 'io-qa')]) {
@@ -78,7 +78,7 @@ def execute() {
 						}
 					}
 				}
-			}
+			
 		}
     
 		post { 
